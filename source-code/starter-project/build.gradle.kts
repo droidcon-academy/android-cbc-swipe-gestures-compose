f7ba88a782a8f111b3d98de1917d13ea7dd19dae
Bkinya <gitongakinya1@gmail.com>
2023-11-07 15:59:26 +0300

    update starter and final projects

--- source-code/starter-project/build.gradle.kts
+++ source-code/starter-project/build.gradle.kts
@@ -1,4 +1,7 @@
+buildscript {
+    val agp_version by extra("8.1.2")
+}
 plugins {
-    id("com.android.application") version "8.2.0-rc01" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
